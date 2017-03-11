e6cbe4dcebf39cbd29ad7c3dba78e5241ff6a40f
Dmitry Smolyakov <dm.smolyakov@gmail.com>
2017-03-12 00:24:11 +0300

    Sample

---
+++ Sample/MySDKTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySDKTestProject
+//
+//  Created by Dmitry Smolyakov on 3/11/17.
+//  Copyright © 2017 Dmitry Smolyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Sample/Pods/Target Support Files/Pods-MySDKTestProject/Pods-MySDKTestProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MySDKTestProject : NSObject
+@end
+@implementation PodsDummy_Pods_MySDKTestProject
+@end
